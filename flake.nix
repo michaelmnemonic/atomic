d063c4d5c21309a59f53799427cc13c676e593eb
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-04-06 00:37:55 +0200

    Add basic flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.pkgs.mkShell {
+      buildInputs = with nixpkgs.legacyPackages.x86_64-linux.pkgs; [
+        mkosi
+        debootstrap
+      ];
+    };
+
+    devShell.aarch64-linux = nixpkgs.legacyPackages.aarch64-linux.pkgs.mkShell {
+      buildInputs = with nixpkgs.legacyPackages.aarch64-linux.pkgs; [
+        mkosi
+        debootstrap
+      ];
+    };
+  };
+}
