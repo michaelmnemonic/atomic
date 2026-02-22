cb627ffe555e52b53f55c5ef1682111caa357170
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-02-22 15:07:12 +0100

    Provide a devshell for debian build

--- flake.nix
+++ flake.nix
@@ -14,11 +14,21 @@
       ];
     };
 
-    devShell.aarch64-linux = nixpkgs.legacyPackages.aarch64-linux.pkgs.mkShell {
-      buildInputs = with nixpkgs.legacyPackages.aarch64-linux.pkgs; [
-        mkosi
+    devShell.aarch64-linux = (nixpkgs.legacyPackages.aarch64-linux.pkgs.buildFHSEnv {
+      name ="mkosi-env";
+      targetPkgs = pkgs: (with pkgs; [
+        apt
         debootstrap
-      ];
-    };
+        dpkg
+        e2fsprogs
+        erofs-utils
+        gnupg
+        mkosi
+      ]);
+      extraBuildCommands = ''
+        ln -s ${./keyrings} $out/usr/share/keyrings
+      '';
+      runScript = "bash";
+    }).env;
   };
 }
